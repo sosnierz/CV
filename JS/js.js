24a15d89ec67a326ae3cec2f87d54d9bbf03b2f4
sosnierz <37373004+sosnierz@users.noreply.github.com>
2022-01-15 20:52:22 +0100

    main

--- JS/js.js
+++ JS/js.js
@@ -1,34 +1,110 @@
 // active nav when web is scroll
-console.log('k')
- function changeNav() {
-    const scrollValue = $(document).scrollTop();
-    const height2 = $('section.about-me').offset().top;
-    const height3 = $('section.projects').offset().top;
-    const height4 = $('section.skills').offset().top;
-    const height5 = $('section.experiences').offset().top;
-    const height6 = $('section.contact').offset().top;
+console.log('k');
+const topSection3 = $('section.projects')
 
-    console.log(scrollValue.toFixed(), height2.toFixed(), height3.toFixed(), height4.toFixed())
+ $(document).on('scroll', function(){
 
-    if (scrollValue < height2) {
-        $('nav.nav-menu li a').not('nav.nav-menu li a.nav0').removeClass('activeNav');
-        $('nav.nav-menu li a.nav0').addClass('activeNav');
-    } else if (scrollValue < height3) {
-        $('nav.nav-menu li a').not('nav.nav-menu li a.nav1').removeClass('activeNav');
-        $('nav.nav-menu li a.nav1').addClass('activeNav');
-    } else if (scrollValue < height4) {
-        $('nav.nav-menu li a').not('nav.nav-menu li a.nav2').removeClass('activeNav');
-        $('nav.nav-menu li a.nav2').addClass('activeNav');
-    } else if (scrollValue < height5) {
-        $('nav.nav-menu li a').not('nav.nav-menu li a.nav3').removeClass('activeNav');
-        $('nav.nav-menu li a.nav3').addClass('activeNav');
-    }   else if (scrollValue < height6) {
-        $('nav.nav-menu li a').not('nav.nav-menu li a.nav4').removeClass('activeNav');
-        $('nav.nav-menu li a.nav4').addClass('activeNav');
-    } else {
-        $('nav.nav-menu li a').not('nav.nav-menu li a.nav5').removeClass('activeNav');
-        $('nav.nav-menu li a.nav5').addClass('activeNav');
-    }
+// all sections
+const windowHeight = $(window).height();
+const scrollValue = $(this).scrollTop();
+const topSection2 = $('section.about-me').offset().top;
+const sectionHeight2 = $('section.about-me').outerHeight();
+const topSection3 = $('section.projects').offset().top;
+const sectionHeight3 = $('section.projects').outerHeight();
+const topSection4 = $('section.skills').offset().top;
+const sectionHeight4 = $('section.skills').outerHeight();
+const topSection5 = $('section.experiences').offset().top;
+const sectionHeight5 = $('section.experiences').outerHeight();
+
+// left navigation scroll 
+if(scrollValue< 500){
+    $('.nav-menu a').not('.nav-menu a.nav0').removeClass('activeNav');
+    $('.nav-menu a.nav0').addClass('activeNav');    
 }
- 
-$(document).on("scroll", changeNav)
+if(scrollValue > topSection2 +sectionHeight2 - windowHeight) {
+    $('.nav-menu a').not('.nav-menu a.nav1').removeClass('activeNav');
+    $('.nav-menu a.nav1').addClass('activeNav');   
+}
+if(scrollValue > topSection3 +sectionHeight3 - windowHeight) {
+    $('.nav-menu a').not('.nav-menu a.nav2').removeClass('activeNav');
+    $('.nav-menu a.nav2').addClass('activeNav');   
+
+}
+if(scrollValue > topSection4 +sectionHeight4 - windowHeight) {
+    $('.nav-menu a').not('.nav-menu a.nav3').removeClass('activeNav');
+    $('.nav-menu a.nav3').addClass('activeNav');   
+}
+if(scrollValue > topSection5 +sectionHeight5 - windowHeight) {
+    $('.nav-menu a').not('.nav-menu a.nav4').removeClass('activeNav');
+    $('.nav-menu a.nav4').addClass('activeNav');   
+    $('.nav-menu a.nav5').addClass('activeNav');   
+}
+
+
+// all projects
+const $hall = $('.project.hall');
+const topProjHall = $hall.offset().top;
+const hallHeight = $hall.outerHeight();
+const $shop = $('.project.shop');
+const topProjShop = $shop.offset().top;
+const shopHeight = $shop.outerHeight();
+const $cv = $('.project.cv')
+const topProjCv = $cv.offset().top;
+const cvHeight = $cv.outerHeight();
+
+// projects show up
+if (scrollValue > topProjHall + hallHeight - windowHeight) {
+    $hall.addClass('active');
+}
+if (scrollValue > topProjShop + shopHeight - windowHeight) {
+    $shop.addClass('active');
+}
+if (scrollValue > topProjCv + cvHeight - windowHeight) {
+    $cv.addClass('active');
+}
+
+if (scrollValue < 1000) {
+    $('project').removeClass('active');
+}
+
+// visible skills
+const topSkills = $('section.skills').offset().top;
+const skillsHeight = $('section.skills').outerHeight();
+const $liSkills = $('.skills ul li');
+if (scrollValue > topSkills + skillsHeight - windowHeight) {
+    $liSkills.each(function(i){
+        setTimeout(() => { 
+            $(this).attr("style", "visibility: visible ")
+        }, i*300);
+       
+    })
+}
+
+// experiences show
+const topExper = $('section.experiences').offset().top;
+const expersHeight = $('section.experiences').outerHeight();
+const $experience = $('article.experience');
+if (scrollValue > topExper+ expersHeight - windowHeight) {
+    $experience.each(function(i){
+        setTimeout(() => { 
+            $(this).attr("style", "transform: scale(1) ")
+        }, i*300);
+       
+    })
+
+}
+
+ })
+
+
+// click navigation
+$('.nav-menu a').on('click', function () {
+    const goTo = "[data-section=" + $(this).attr('class') + "]";
+    $("html,body").animate({
+        scrollTop: $(goTo).offset().top
+    })
+})
+
+
+
+    
